b3b0361321155017de2e25e0e60ddd32642c617a
jesus duran <jesus.duran@wiedii.co>
2024-09-18 16:18:04 -0500

    segundo commit

---
+++ tipos/strings.js
@@ -0,0 +1,8 @@
+"use strict";
+(() => {
+    var _a;
+    const batman = 'batman';
+    const linternaVerde = "linterna's verde";
+    const volcanNegro = 'Heroe volcan negro';
+    console.log(((_a = batman[7]) === null || _a === void 0 ? void 0 : _a.toUpperCase()) || "no esta presente");
+})();
---
+++ tipos/strings.ts
@@ -0,0 +1,9 @@
+(()=>{
+
+    const batman: string = 'batman';
+    const linternaVerde: string = "linterna's verde";
+    const volcanNegro: string = 'Heroe volcan negro';
+
+    console.log(batman[7]?.toUpperCase() || "no esta presente");
+
+ })() 
